b5326806250c7c79907208151416be8cf59fd777
Albin Médoc <albinmedoc99@gmail.com>
2019-03-31 01:03:15 +0100

    Förbättrade tidslinjen, gjorde så den placerades i mitten(vertikalt) samt att man inte behöver dra endast ovanför tidslinjen

--- js/timeline.js
+++ js/timeline.js
@@ -1,27 +1,27 @@
 $(document).ready(function(){
-
     //-- Tidslinje dragbar --
     //lastposition och position ökar med 70 eftersom den är 70px ifrån toppen av sidan
+
     var dragging = false, lastPosition, position;
-    $(".timeline").on("mousedown mouseup mousemove", function(e){
-        if(e.type == "mousedown"){
-            //Användaren trycker ner musen
-            dragging = true;
-            lastPosition = e.clientY + $(".timeline").scrollTop() + 70;
-        }
-        if(e.type == "mouseup"){
-            //Användaren släppte musen
-            dragging = false;
-        }
-        if(e.type == "mousemove" && dragging == true){
-            //Användaren drar i tidslinjen
-            position = e.clientY + $(".timeline").scrollTop() + 70;
-            $(".timeline").scrollTop($(".timeline").scrollTop() - (position - lastPosition));
-            //Sätter jag "lastPosition = position;" blir det laggigt att dra tidslinjen
-            lastPosition = e.clientY + $(".timeline").scrollTop() + 70;
-        }
+
+    $(".timeline_container").mousedown(function(e){
+        //Användaren trycker ner musen när den är över tidslinjen
+        dragging = true;
+        lastPosition = e.clientY + $(".timeline_container").scrollTop() + 70;
     });
-    $(window).on("mouseup", function(){
+
+    $(window).mouseup(function(){
+        //Användaren släpper musen
         dragging = false;
+    });
+
+    $(window).mousemove(function(e){
+        if(dragging){
+            //Användaren drar i tidslinjen
+            position = e.clientY + $(".timeline_container").scrollTop() + 70;
+            $(".timeline_container").scrollTop($(".timeline_container").scrollTop() - (position - lastPosition));
+            //Sätter jag "lastPosition = position;" blir det laggigt att dra tidslinjen
+            lastPosition = e.clientY + $(".timeline_container").scrollTop() + 70;
+        }
     });
 });
