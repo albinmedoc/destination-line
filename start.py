54cd68b61b1ab533d25dba477200cbce82e0b4bf
Albin Médoc <albinmedoc99@gmail.com>
2019-04-01 14:40:58 +0200

    Fixade python server

---
+++ start.py
@@ -0,0 +1,18 @@
+from flask import Flask, session, render_template, request, url_for, redirect
+from os import urandom
+
+DEBUG_MODE = True
+
+app = Flask(__name__)
+app.secret_key = urandom(24)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/timeline")
+def timeline():
+    return render_template("timeline.html")
+
+if __name__ == "__main__":
+    app.run(host= "192.168.1.18", port=80, debug=DEBUG_MODE)
---
+++ static/js/timeline.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    //-- Tidslinje dragbar --
+
+    var dragging = false, lastPositionX, lastPositionY, positionX, positionY;
+
+    $(".timeline_container").mousedown(function(e){
+        //Användaren trycker ner musen när den är över tidslinjen
+        dragging = true;
+        lastPositionX = e.clientX + $(".timeline_container").scrollLeft();
+        lastPositionY = e.clientY + $(".timeline_container").scrollTop();
+    });
+
+    $(window).mouseup(function(){
+        //Användaren släpper musen
+        dragging = false;
+    });
+
+    $(window).mousemove(function(e){
+        if(dragging){
+            //Användaren drar i tidslinjen
+            positionX = e.clientX + $(".timeline_container").scrollLeft();
+            positionY = e.clientY + $(".timeline_container").scrollTop();
+
+            //Scrolla beroende på hur mycket användaren har dragit
+            $(".timeline_container").scrollLeft($(".timeline_container").scrollLeft() - (positionX - lastPositionX));
+            $(".timeline_container").scrollTop($(".timeline_container").scrollTop() - (positionY - lastPositionY));
+
+            //Sätter jag "lastPosition = position;" blir det laggigt att dra tidslinjen
+            lastPositionX = e.clientX + $(".timeline_container").scrollLeft();
+            lastPositionY = e.clientY + $(".timeline_container").scrollTop();
+        }
+    });
+});
