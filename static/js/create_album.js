1d6c9f7357a676b51df9a81cb6882d358dad9490
Albin Médoc <albinmedoc99@gmail.com>
2019-04-09 22:50:10 +0200

    Påbörjade att man kan flytta poster när man skapar ett inlägg. Använda ramverket SortableJS.

--- static/js/create_album.js
+++ static/js/create_album.js
@@ -1,3 +1,10 @@
 $("#upload_btn").click(function(){
     $("#upload").trigger("click");
 });
+
+Sortable.create(test, {
+    animation: 500,
+    draggable: ".post",
+    scroll: true,
+	scrollSensitivity: 50
+  });
