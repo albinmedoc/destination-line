0f35e39f931d8e23860567dc21ffe2fa588d50dc
Albin Médoc <albinmedoc99@gmail.com>
2019-04-11 23:52:44 +0200

    Påbörjade att bilder visas innan man har laddat upp dem, preview och ordning. (Skapa album sida)

--- static/js/create_album.js
+++ static/js/create_album.js
@@ -1,10 +1,54 @@
-$("#upload_btn").click(function(){
-    $("#upload").trigger("click");
-});
+$(document).ready(function(){
+    var images = [];
 
-Sortable.create(test, {
-    animation: 500,
-    draggable: ".post",
-    scroll: true,
-	scrollSensitivity: 50
-  });
+    $("#upload_btn").click(function(){
+        $("#upload").trigger("click");
+    });
+
+    $("#upload").change(function(){
+        if(this.files && this.files.length <= 60){
+            for(var i = 0; i < this.files.length; i++){
+                var file = this.files[i];
+                if(file.type.match("image.*")){
+                    var reader = new FileReader();
+                    reader.onload = (function(file){
+                        return function(e){
+                            if(images.indexOf(e.target.result) < 0){
+                                images.push(e.target.result);
+                                var post = "<div class='post'><i class='material-icons close'>close</i><img src='" + e.target.result + "'><i class='material-icons info'>list</i>";
+                                $("#upload_btn").after(post);
+                            }else{
+                                alert("The image " + file.name + " has already been uploaded. Skipping..")
+                            }
+                        }
+                    })(file);
+                    reader.readAsDataURL(file);
+                    console.log(file);
+                }else{
+                    alert("The file " + file.name + " is not an image. Skipping..");
+                }
+            }
+        }
+    });
+
+    $("#upload_btn").parent().on("click", ".post > i.close", function(){
+        var index = $(this).next().attr("src");
+        console.log(index);
+        images.splice(images.indexOf(index), 1);
+        $(this).parent().remove();
+    });
+
+    $("#upload_form").on("submit", function(e){
+        e.preventDefault();
+        console.log(images);
+
+    });
+
+    Sortable.create(test, {
+        animation: 500,
+        draggable: ".post",
+        scroll: true,
+        scrollSensitivity: 50,
+    });
+  
+});
--- static/js/login_register.js
+++ static/js/login_register.js
@@ -1,77 +1,77 @@
 
-    $("#login, #register").mousedown(function(e){
-        if(e.target.id == "login" || e.target.id == "register"){
-            location.hash = '';
-        }
-    });
+$("#login, #register").mousedown(function(e){
+    if(e.target.id == "login" || e.target.id == "register"){
+        location.hash = '';
+    }
+});
 
-    $("#register > fieldset > .form_row > .input_container > input[name='password2']").keyup(function(){
-        //Kontrollerar att password2 stämmer överrens med password2
-        var pass1 = $("#register > fieldset > .form_row > .input_container > input[name='password']").val()
-        var pass2 = $(this).val();
-        if($(this).val() != "" && pass1 != pass2){
-            $(this).addClass("error");
+$("#register > fieldset > .form_row > .input_container > input[name='password2']").keyup(function(){
+    //Kontrollerar att password2 stämmer överrens med password2
+    var pass1 = $("#register > fieldset > .form_row > .input_container > input[name='password']").val()
+    var pass2 = $(this).val();
+    if($(this).val() != "" && pass1 != pass2){
+        $(this).addClass("error");
+    }else{
+        $(this).removeClass("error");
+    }
+});
+
+//Kontrollerar om username redan finns
+$("#register > fieldset > .form_row > .input_container > input[name='username']").focusout(function(){
+    $.ajax({
+        method: "POST",
+        url: $SCRIPT_ROOT + "/request",
+        data: {
+            request: "username_exists",
+            username: $(this).val()
+        }
+    })
+    .done(function(exist){
+        if(exist){
+            $("#register > fieldset > .form_row > .input_container > input[name='username']").addClass("error");
         }else{
-            $(this).removeClass("error");
+            $("#register > fieldset > .form_row > .input_container > input[name='username']").removeClass("error");
         }
     });
+});
 
-    //Kontrollerar om username redan finns
-    $("#register > fieldset > .form_row > .input_container > input[name='username']").focusout(function(){
-        $.ajax({
-            method: "POST",
-            url: $SCRIPT_ROOT + "/request",
-            data: {
-                request: "username_exists",
-                username: $(this).val()
-            }
-        })
-        .done(function(exist){
-            if(exist){
-                $("#register > fieldset > .form_row > .input_container > input[name='username']").addClass("error");
-            }else{
-                $("#register > fieldset > .form_row > .input_container > input[name='username']").removeClass("error");
-            }
-        });
+//Kontrollerar om email redan finns
+$("#register > fieldset > .form_row > .input_container > input[name='email']").focusout(function(){
+    $.ajax({
+        method: "POST",
+        url: $SCRIPT_ROOT + "/request",
+        data: {
+            request: "email_exists",
+            email: $(this).val()
+        }
+    })
+    .done(function(exist){
+        if(exist){
+            $("#register > fieldset > .form_row > .input_container > input[name='email']").addClass("error");
+        }else{
+            $("#register > fieldset > .form_row > .input_container > input[name='email']").removeClass("error");
+        }
     });
+});
 
-    //Kontrollerar om email redan finns
-    $("#register > fieldset > .form_row > .input_container > input[name='email']").focusout(function(){
-        $.ajax({
-            method: "POST",
-            url: $SCRIPT_ROOT + "/request",
-            data: {
-                request: "email_exists",
-                email: $(this).val()
-            }
-        })
-        .done(function(exist){
-            if(exist){
-                $("#register > fieldset > .form_row > .input_container > input[name='email']").addClass("error");
-            }else{
-                $("#register > fieldset > .form_row > .input_container > input[name='email']").removeClass("error");
-            }
-        });
+
+$('#login').on('submit', function(e) {
+    $.ajax({
+        method: "POST",
+        url: $SCRIPT_ROOT + "/login",
+        data: {
+            username: $("#login > fieldset > .form_row > .input_container > input[name='username']").val(),
+            password: $("#login > fieldset > .form_row > .input_container > input[name='password']").val()
+        }
+    })
+    .done(function(valid){
+        if(!valid){
+            $("#login > fieldset > .form_row > .input_container > input[name='username']").addClass("error");
+            $("#login > fieldset > .form_row > .input_container > input[name='password']").addClass("error");
+
+        }else{
+            location.reload();
+        }
     });
-
-
-    $('#login').on('submit', function(e) {
-        $.ajax({
-            method: "POST",
-            url: $SCRIPT_ROOT + "/login",
-            data: {
-                username: $("#login > fieldset > .form_row > .input_container > input[name='username']").val(),
-                password: $("#login > fieldset > .form_row > .input_container > input[name='password']").val()
-            }
-        })
-        .done(function(valid){
-            if(!valid){
-                $("#login > fieldset > .form_row > .input_container > input[name='username']").addClass("error");
-                $("#login > fieldset > .form_row > .input_container > input[name='password']").addClass("error");
-
-            }else{
-                location.reload();
-            }
-        });
-        e.preventDefault();
-    });
+    e.preventDefault();
+});
