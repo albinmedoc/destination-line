760809cc8f44439032de5c9ce16129949c0fa800
Albin Medoc <albinmedoc@gmail.com>
2019-04-12 18:59:19 +0200

    Byte namn från Upload.py till Image.py

--- Image.py
+++ Image.py
@@ -2,9 +2,9 @@ from flask import Blueprint, request, session, render_template, json
 import os
 from werkzeug.utils import secure_filename
 
-ALLOWED_EXTENSIONS = set(["pdf", "png", "jpg", "jpeg"])
+app = Blueprint("image", __name__, template_folder="templates")
 
-app = Blueprint("upload", __name__, template_folder="templates")
+ALLOWED_EXTENSIONS = set(["pdf", "png", "jpg", "jpeg"])
 
 @app.route("/upload", methods = ["GET", "POST"])
 def upload():
@@ -13,6 +13,10 @@ def upload():
                         return "<h1>Du måste vara inloggad</h1>"
                 return render_template("create_album.html")
         
+        print(request.form.get("country"))
+        print(request.form.get("city"))
+        print(request.form.get("date_start"))
+        print(request.form.get("date_end"))
         for key in request.files:
                 file = request.files[key]
                 if(validate_image(file)):
@@ -21,8 +25,12 @@ def upload():
                                 print("Images folder don´t exist. Creating one..")
                                 os.makedirs(directory)
                         path = os.path.join(directory, secure_filename(file.filename))
-                        print("Image saved to: " + path)
-                        file.save(path)
+                        try:
+                                file.save(path)
+                                print("Image saved to: " + path)
+                        except:
+                                print("Image " + file.filename + " couldn´t be saved. Skipping..")
+
         return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
 
 #Kollar så filen inte är tom och har rätt filendelse.
