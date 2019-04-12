ac107edd29ad638a595351888bae36e1b003faea
Albin Medoc <albinmedoc@gmail.com>
2019-04-12 19:13:48 +0200

    Uppladade bilder kan visas genom en länk

--- Image.py
+++ Image.py
@@ -1,10 +1,13 @@
-from flask import Blueprint, request, session, render_template, json
+from flask import Blueprint, request, session, render_template, json, send_from_directory
 import os
 from werkzeug.utils import secure_filename
 
 app = Blueprint("image", __name__, template_folder="templates")
 
 ALLOWED_EXTENSIONS = set(["pdf", "png", "jpg", "jpeg"])
+
+UPLOAD_FOLDER = os.path.join(os.path.abspath(os.curdir) + "/images")
+
 
 @app.route("/upload", methods = ["GET", "POST"])
 def upload():
@@ -20,11 +23,10 @@ def upload():
         for key in request.files:
                 file = request.files[key]
                 if(validate_image(file)):
-                        directory = os.path.join(os.path.abspath(os.curdir) + "/images")
-                        if(not os.path.exists(directory)):
+                        if(not os.path.exists(UPLOAD_FOLDER)):
                                 print("Images folder don´t exist. Creating one..")
-                                os.makedirs(directory)
-                        path = os.path.join(directory, secure_filename(file.filename))
+                                os.makedirs(UPLOAD_FOLDER)
+                        path = os.path.join(UPLOAD_FOLDER, secure_filename(file.filename))
                         try:
                                 file.save(path)
                                 print("Image saved to: " + path)
@@ -32,6 +34,11 @@ def upload():
                                 print("Image " + file.filename + " couldn´t be saved. Skipping..")
 
         return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+
+@app.route("/image/<image_id>", methods = ["GET"])
+def uploaded_images(image_id):
+    return send_from_directory(UPLOAD_FOLDER, image_id)
+
 
 #Kollar så filen inte är tom och har rätt filendelse.
 def validate_image(file):
