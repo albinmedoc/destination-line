c80d59cc5f5f76123a2547917f4e358bc54de868
Albin Medoc <albinmedoc@gmail.com>
2019-04-14 00:13:30 +0200

    Routes importeras från User

--- start.py
+++ start.py
@@ -1,9 +1,9 @@
 # coding: UTF-8
 import psycopg2
-from flask import Flask, flash, session, render_template, request, url_for, redirect, jsonify
+from flask import Flask, flash, render_template
 from os import urandom
 from sys import argv
-import User, Image, ajax_requests
+import User, Image
 
 SERVER_IP = "192.168.1.18"
 
@@ -19,39 +19,6 @@ def index():
         flash(u'This website uses cookies', 'cookie')
         return render_template("index.html")
 
-@app.route("/login", methods = ["POST"])
-def login():
-        username = request.form.get("username")
-        password = request.form.get("password")
-        
-        
-        if(username.strip() and password.strip()):
-                if(User.check_password(password, username = username)):
-                        session["username"] = username
-                        return jsonify(True)
-        return jsonify(False)
-
-@app.route("/logout")
-def logout():
-        session.clear()
-        return redirect("/")
-
-@app.route("/register", methods = ["POST"])
-def register():
-        firstname = request.form.get("firstname")
-        lastname = request.form.get("lastname")
-        username = request.form.get("username")
-        email = request.form.get("email")
-        password = request.form.get("password")
-        password2 = request.form.get("password2")
-        if(password == password2):
-                if(User.create_user(firstname, lastname, username, email, password)):
-                        session["username"] = username
-                        return redirect("/")
-                else:
-                        return "Något gick fel"
-        return "password stämmer inte överrens"
-
 @app.route("/profile/<username>")
 def profile(username):
         # Kolla om användaren besöker sin egna profil
@@ -63,7 +30,7 @@ def timeline():
 
 if __name__ == "__main__":
         #Importera Blueprints
-        app.register_blueprint(ajax_requests.app)
+        app.register_blueprint(User.app)
         app.register_blueprint(Image.app)
         #Startar servern på olika adresser beroende på attribut
         if(len(argv) > 1 and argv[1].lower() == "server"):
