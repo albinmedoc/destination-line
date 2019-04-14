eb3a762b2d1bb99ee25a330f456270ee470215fd
Albin Medoc <albinmedoc@gmail.com>
2019-04-14 18:20:24 +0200

    Album skapas i databas, samt poster med rätt id till album

--- Image.py
+++ Image.py
@@ -4,6 +4,7 @@ from uuid import uuid4
 from werkzeug.utils import secure_filename
 from PIL import Image
 from Database import Databse
+from datetime import datetime
 
 app = Blueprint("image", __name__, template_folder="templates")
 
@@ -12,21 +13,23 @@ ALLOWED_EXTENSIONS = set(["pdf", "png", "jpg", "jpeg", "webp"])
 UPLOAD_FOLDER = os.path.join(os.path.abspath(os.curdir) + "/images")
 
 
-@app.route("/upload", methods = ["GET", "POST"])
+@app.route("/new/album", methods = ["GET", "POST"])
 def upload():
         if(request.method == "GET"):
                 if("username" not in session):
                         return "<h1>Du måste vara inloggad</h1>"
                 return render_template("create_album.html")
         
-        print(request.form.get("country"))
-        print(request.form.get("city"))
-        print(request.form.get("date_start"))
-        print(request.form.get("date_end"))
+        country = request.form.get("country")
+        city = request.form.get("city")
+        date_start = datetime.strptime(request.form.get("date_start"), "%Y-%m-%d")
+        date_end = datetime.strptime(request.form.get("date_end"), "%Y-%m-%d")
+
         #Fixa så man kollar om filer och information skickades med
         db = Databse()
         cur = db.conn.cursor()
-        
+        cur.execute("insert into album(published, country, city, date_start, date_end) values(%s, %s, %s, %s, %s) returning id", (datetime.utcnow(), country, city, date_start, date_end))
+        album_id = cur.fetchone()[0]
         for key in request.files:
                 file = request.files[key]
                 if(validate_image(file)):
@@ -40,7 +43,7 @@ def upload():
                         while os.path.isfile(filename):
                                 filename = str(uuid4()) + ".webp"
                         img.save(os.path.join(UPLOAD_FOLDER, secure_filename(filename)))
-                        cur.execute("insert into post(album, index, img_name, text) values(1, 1, %s, %s)", (filename, "This is Destination Lines first image."))
+                        cur.execute("insert into post(album, index, img_name, text) values(%s, 1, %s, %s)", (album_id, filename, "This is Destination Lines first image."))
         db.conn.commit()
         return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
 
