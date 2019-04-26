3b01f05f131406590df31a8b0818faf5981a5423
Albin Médoc <albinmedoc99@gmail.com>
2019-04-27 01:16:14 +0200

    Flyttade saker till Image.py

--- start.py
+++ start.py
@@ -5,17 +5,13 @@ from os import urandom
 from sys import argv
 import User, Image
 from Settings import SERVER_IP, SERVER_PORT, DEBUG_MODE
-from Database import Database
 
 app = Flask(__name__)
 app.secret_key = urandom(24)
 
 @app.route("/")
 def index():
-        db = Database()
-        cur = db.conn.cursor()
-        cur.execute("select album.city, album.country, person.firstname, person.lastname, post.img_name from (album join post on album.id=post.album) join person on album.owner=person.id where post.index=1 order by album.published limit 50")
-        albums = cur.fetchall()
+        albums = Image.get_new_albums()
         if(albums is not None):
                 return render_template("index.html", albums=albums)
         return render_template("index.html")
