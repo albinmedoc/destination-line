bd94a2f3a68e9e15d7fe3e26a10125e13f80b468
Daniel Subasic <subasic97@gmail.com>
2019-05-12 16:07:14 +0200

    Om sökrutan är tom så rensas sökresultaten

--- static/js/search.js
+++ static/js/search.js
@@ -1,6 +1,6 @@
 //Kontrollerar om username redan finns
 var timeout;
-$('#search').keypress(function() {
+$('#search').keyup(function() {
     if(timeout) {
         clearTimeout(timeout);
         timeout = null;
@@ -9,31 +9,36 @@ $('#search').keypress(function() {
     timeout = setTimeout(do_search, 500)
 })
 var do_search = function(){
-    $('.loader_container').addClass('is_visible');
-    $.ajax({
-        method: "POST",
-        url: $SCRIPT_ROOT + "/request/search",
-        data: {
-            search: $("#search").val()
-        },
-        timeout: 35000
-    })
-    .done(function(data){
+    if ($("#search").val() != ""){
+        $('nav .loader_container').addClass('is_visible');
+        $.ajax({
+            method: "POST",
+            url: $SCRIPT_ROOT + "/request/search",
+            data: {
+                search: $("#search").val()
+            },
+            timeout: 35000
+        })
+        .done(function(data){
+            $(".search_category").html('');
+            if(data.countries){
+                $("#countries").append("<h2>Destinations</h2>");
+                for (country in data.countries){
+                    $("#countries").append("<div class='search_result'><img src='https://i.imgur.com/azjwFrj.jpg'><div class='user_search_info'><span>"+data.countries[country][2]+"</span><a href='/album/"+data.countries[country][0]+"' class='city'><h3>"+data.countries[country][3]+"</h3></a></div></div>")
+                }
+            }
+            if(data.users){
+                $("#users").append("<h2>Users</h2>");
+                for (user in data.users){
+                    $("#users").append("<div class='search_result'><img src='https://i.imgur.com/azjwFrj.jpg'><div class='user_search_info'><span>"+data.users[user][2]+" "+data.users[user][3]+"</span><a href='/profile/"+data.users[user][1]+"' class='username'><span>"+data.users[user][1]+"</span></a><div class='follow_info'><a><b>3</b> Followers</a><a><b>24</b> Following</a></div></div></div>")
+                }
+            }
+            $('nav .loader_container').removeClass('is_visible');
+        });
+    }
+    else{
         $(".search_category").html('');
-        if(data.countries){
-            $("#countries").append("<h2>Destinations</h2>");
-            for (country in data.countries){
-                $("#countries").append("<div class='search_result'><img src='https://i.imgur.com/azjwFrj.jpg'><div class='user_search_info'><span>"+data.countries[country][2]+"</span><a href='/album/"+data.countries[country][0]+"' class='city'><h3>"+data.countries[country][3]+"</h3></a></div></div>")
-            }
-        }
-        if(data.users){
-            $("#users").append("<h2>Users</h2>");
-            for (user in data.users){
-                $("#users").append("<div class='search_result'><img src='https://i.imgur.com/azjwFrj.jpg'><div class='user_search_info'><span>"+data.users[user][2]+" "+data.users[user][3]+"</span><a href='/profile/"+data.users[user][1]+"' class='username'><span>"+data.users[user][1]+"</span></a><div class='follow_info'><a><b>3</b> Followers</a><a><b>24</b> Following</a></div></div></div>")
-            }
-        }
-        $('.loader_container').removeClass('is_visible');
-    });
+    }
 };
 
 $("#search_container>a").click(function(){
