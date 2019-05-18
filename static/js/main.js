ffbe1d83dcca23adc7a04764a0ee93f2122ed7f0
Albin Médoc <albinmedoc99@gmail.com>
2019-05-19 01:46:51 +0200

    Tog bort kod som inte fungerade

--- static/js/main.js
+++ static/js/main.js
@@ -7,11 +7,6 @@ window.mobileAndTabletcheck = function () {
 };
 
 window.onload = function () {
-    $("#login, #register").mousedown(function (e) {
-        if (e.target.id == "login" || e.target.id == "register") {
-            location.hash = '';
-        }
-    });
     $(".close_flash").click(function (e) {
         $(this).closest('div').remove();
     });
