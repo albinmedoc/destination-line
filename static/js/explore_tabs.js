cbe4cadd8d1ea7e79cc1022f73e179ce7863ebdb
albinmedoc <stamline203@gmail.com>
2019-05-19 23:27:25 +0200

    Lade till att hemsidan scrollas till toppen när sektion ändras

--- static/js/explore_tabs.js
+++ static/js/explore_tabs.js
@@ -6,20 +6,23 @@ var diffrence = 0;
 window.onscroll = function(){
     var currentScrollPos = window.pageYOffset;
     diffrence = diffrence + (prevScrollpos-currentScrollPos);
+    //Användaren skrollar nedåt
     if (prevScrollpos > currentScrollPos) {
         if(diffrence <= -200){
             diffrence = 0;
         }
         if(diffrence >= 200){
-            $("nav").addClass("is_visible");
+            $("body > nav").addClass("is_visible");
             $("#explore_tabs").addClass("is_visible");
         }
-    } else {
+    }
+    //Användaren skrollar uppåt
+    else {
         if(diffrence >= 200){
             diffrence = 0;
         }
         if(diffrence <= -200){
-            $("nav").removeClass("is_visible");
+            $("body > nav").removeClass("is_visible");
             $("#explore_tabs").removeClass("is_visible");
         }
     }
@@ -30,15 +33,22 @@ window.onload = function(){
     $("#explore_tabs > .explore_tab").click(function(){
         //Klickar på following
         if($(this).hasClass("following")){
+            //Visar following-sektion och döljer explore-sektion
             $("#explore").hide();
             $("#following").show();
         }
         //Klickar på explore
         else{
+            //Visar explore-sektion och döljer following-sektion
             $("#following").hide();
             $("#explore").show();
         }
         //Bytar plats på active-class
         $("#explore_tabs > .explore_tab").toggleClass("active");
+        //Scrollar till toppen
+        $("html, body").animate({ scrollTop: 0 }, "fast");
+        //Visar både nav och explore_tabs
+        $("body > nav").addClass("is_visible");
+        $("#explore_tabs").addClass("is_visible");
     });
 }
