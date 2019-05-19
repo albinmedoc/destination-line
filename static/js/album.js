e530f30e1f12adf402d3d4155e6c7a0611dabc87
Daniel Subasic <subasic97@gmail.com>
2019-05-19 21:31:28 +0200

    La till bläddring av inlägg (bilder och text)

--- static/js/album.js
+++ static/js/album.js
@@ -1,38 +1,38 @@
-window.onload = function() {
-    $(".flex_font").each(function(){
-        if($(this).text().length > 9 && $(this).text().length < 15){
-            if($(this).hasClass("country")){
-                $(this).css("font-size", "3.5vw");
-            }else if($(this).hasClass("city")){
-                $(this).css("font-size", "7.2vw");
-            }
-        }else if($(this).text().length >= 15 && $(this).text().length < 20){
-            if($(this).hasClass("country")){
-                $(this).css("font-size", "3.2vw");
-            }else if($(this).hasClass("city")){
-                $(this).css("font-size", "5vw");
-            }
-        }
-        else if($(this).text().length >= 20 && $(this).text().length < 25){
-            if($(this).hasClass("country")){
-                $(this).css("font-size", "2.9vw");
-            }else if($(this).hasClass("city")){
-                $(this).css("font-size", "4.1vw");
-            }
-        }
-        else if($(this).text().length >= 25 && $(this).text().length < 30){
-            if($(this).hasClass("country")){
-                $(this).css("font-size", "2.7vw");
-            }else if($(this).hasClass("city")){
-                $(this).css("font-size", "3.1vw");
-            }
-        }
-        else if($(this).text().length >= 30 && $(this).text().length < 35){
-            if($(this).hasClass("country")){
-                $(this).css("font-size", "2.5vw");
-            }else if($(this).hasClass("city")){
-                $(this).css("font-size", "2.2vw");
-            }
-        }
-    });
-};
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plus_slides(n) {
+  showSlides(slideIndex += n);
+}
+
+function current_slide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    var description = document.getElementsByClassName("post_text_description");
+    var dots = document.getElementsByClassName("thumbnail");
+    
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        description[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    description[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    }
