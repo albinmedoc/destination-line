acdc02acd2616a708df3fab3600e66004868a086
albinmedoc <stamline203@gmail.com>
2019-05-22 13:52:05 +0200

    Skapade funktion för att visa flash-meddelande

--- static/js/flashes.js
+++ static/js/flashes.js
@@ -13,11 +13,17 @@ $(document).ready(function(){
     //Körs 5 sekunder efter sidan har laddats in
     setTimeout(function(){
         //Ta bort alla flashes
-        $('#flashes').remove();
+        $('#flashes > .flash').remove();
         //Flytta upp navigationen
         offset_nav();
     }, 5000);
 });
+
+function add_flash_message(message, category){
+    var message = $("<div class='flash " + category + "'><i class='close material-icons'>close</i><p>" + message + "</p></div>")
+    $("#flashes").prepend(message);
+    offset_nav();
+}
 
 function offset_nav(){
     //Räknar ut hur mycket navigationen och explore_tabs behövs flyttas ned
