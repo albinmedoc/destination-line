09ee41c9cc95dc0fad09c10411288bef35498669
Daniel Subasic <subasic97@gmail.com>
2019-05-22 23:58:33 +0200

    Löste fel där explore_tabs togglades när de inte ska

--- static/js/explore_tabs.js
+++ static/js/explore_tabs.js
@@ -1,27 +1,27 @@
 var prevScrollpos = window.pageYOffset;
 
 // diffrence < 0 = ner, diffrence > 0 = upp
-var diffrence = 0;
+var difference = 0;
 
 window.onscroll = function(){
     var currentScrollPos = window.pageYOffset;
-    diffrence = diffrence + (prevScrollpos-currentScrollPos);
+    difference = difference + (prevScrollpos-currentScrollPos);
     //Användaren skrollar nedåt
     if (prevScrollpos > currentScrollPos) {
-        if(diffrence <= -200){
-            diffrence = 0;
+        if(difference <= -200){
+            difference = 0;
         }
-        if(diffrence >= 200){
+        if(difference >= 200){
             $("body > nav").addClass("is_visible");
             $("#explore_tabs").addClass("is_visible");
         }
     }
     //Användaren skrollar uppåt
     else {
-        if(diffrence >= 200){
-            diffrence = 0;
+        if(difference >= 200){
+            difference = 0;
         }
-        if(diffrence <= -200){
+        if(difference <= -200){
             $("body > nav").removeClass("is_visible");
             $("#explore_tabs").removeClass("is_visible");
         }
@@ -36,15 +36,17 @@ window.onload = function(){
             //Visar following-sektion och döljer explore-sektion
             $("#explore").hide();
             $("#following").show();
+            $("#explore_tabs > .explore_tab.following, #following").addClass("active");
+            $("#explore_tabs > .explore_tab.explore, #explore").removeClass("active");
         }
         //Klickar på explore
         else{
             //Visar explore-sektion och döljer following-sektion
             $("#following").hide();
             $("#explore").show();
+            $("#explore_tabs > .explore_tab.explore, #explore").addClass("active");
+            $("#explore_tabs > .explore_tab.following, #following").removeClass("active");
         }
-        //Bytar plats på active-class
-        $("#explore_tabs > .explore_tab, #explore, #following").toggleClass("active");
         //Scrollar till toppen
         $("html, body").animate({ scrollTop: 0 }, "fast");
         //Visar både nav och explore_tabs
