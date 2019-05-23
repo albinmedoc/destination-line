3545c64e3ba0763d14d4cc736686555da4c74764
albinmedoc <stamline203@gmail.com>
2019-05-23 20:33:40 +0200

    Lade till funktionalitet för att ändra bakgrundsbild på profilsida

--- static/js/profile.js
+++ static/js/profile.js
@@ -39,14 +39,13 @@ window.onload = function () {
             });
         }
     });
-   //När man klickar på "kameraikonen" kommer datorns filer för att byta profilbild
+   //Öppnar input för att välja profilbild
     $("#change_profile_img_btn").click(function(){
         $("#input_profile_img").trigger("click");
     });
     
     $("#input_profile_img").change(function(){
         var file = this.files[0];
-        console.log(file);
         var data = new FormData();
         data.append("file", file);
 
@@ -61,10 +60,44 @@ window.onload = function () {
                 $('.loader_container').addClass('is_visible');
             },
             success: function(data){
-                location.reload(true);
+                add_flash_message("Profile image changed", "success");
+                $("#profile_img > img").attr("src", $SCRIPT_ROOT + "/image/" + data);
             },
             error: function(data){
                 add_flash_message("Could not change profile image...", "error");
+            },
+            complete: function(){
+                $('.loader_container').removeClass('is_visible');
+            }
+        });
+    });
+
+    //Öppnar input för att välja profilbild
+    $("#change_background_img_btn").click(function(){
+        $("#input_background_img").trigger("click");
+    });
+    
+    $("#input_background_img").change(function(){
+        var file = this.files[0];
+        var data = new FormData();
+        data.append("file", file);
+
+        $.ajax({
+            type: 'POST',
+            url: $SCRIPT_ROOT + "/upload_background_img",
+            data: data,
+            cache: false,
+            contentType: false,
+            processData: false,
+            beforeSend: function(){
+                $('.loader_container').addClass('is_visible');
+            },
+            success: function(data){
+                add_flash_message("Background image changed", "success");
+                $("#profile_header").css("background-image", "url(" + $SCRIPT_ROOT + "/image/" + data + ")");
+            },
+            error: function(data){
+                add_flash_message("Could not change background image...", "error");
             },
             complete: function(){
                 $('.loader_container').removeClass('is_visible');
