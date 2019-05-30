6b40d27d38a20627bbbe6020dd55e920251df0fa
albinmedoc <stamline203@gmail.com>
2019-05-30 14:59:42 +0200

    Lade till kommentarer

--- static/js/nav.js
+++ static/js/nav.js
@@ -5,38 +5,40 @@ $('#search').keyup(function () {
         clearTimeout(timeout);
         timeout = null;
     }
-
     timeout = setTimeout(do_search, 500)
 })
+
 var do_search = function () {
     $("nav .loader_container").addClass("is_visible");
+    //Hämtar sökresultat
     $.ajax({
-            method: "POST",
-            url: $SCRIPT_ROOT + "/request/search",
-            data: {
-                search: $("#search").val()
-            },
-            timeout: 35000
-        })
-        .done(function (data) {
-            $(".search_category").html('');
-            if (data.destinations) {
-                for (destination in data.destinations) {
-                    $("#destinations").append("<div class='search_result'><i class='material-icons-outlined'>place</i><a href='/album/" + data.destinations[destination][0] + "'><img src='/image/" + data.destinations[destination][5] + "'></a><div class='destinations_search_info'><a href='/album/" + data.destinations[destination][0] + "' class='city'><h3>" + data.destinations[destination][3] + "</h3></a><p>" + data.destinations[destination][2] + "</p><a class='album_owner' href='#'><i class='material-icons-outlined'>person</i><span>" + data.destinations[destination][4] + "</span></a></div></div>");
-                }
+        method: "POST",
+        url: $SCRIPT_ROOT + "/request/search",
+        data: {
+            search: $("#search").val()
+        },
+        timeout: 35000
+    })
+    .done(function (data) {
+        $(".search_category").html('');
+        //Visar sökresultat
+        if (data.destinations) {
+            for (destination in data.destinations) {
+                $("#destinations").append("<div class='search_result'><i class='material-icons-outlined'>place</i><a href='/album/" + data.destinations[destination][0] + "'><img src='/image/" + data.destinations[destination][5] + "'></a><div class='destinations_search_info'><a href='/album/" + data.destinations[destination][0] + "' class='city'><h3>" + data.destinations[destination][3] + "</h3></a><p>" + data.destinations[destination][2] + "</p><a class='album_owner' href='#'><i class='material-icons-outlined'>person</i><span>" + data.destinations[destination][4] + "</span></a></div></div>");
             }
-            if (data.users) {
-                for (user in data.users) {
-                    console.log(data.users[user][4]);
-                    var profile_img = $SCRIPT_ROOT + "/static/img/avatar.png";
-                    if (data.users[user][4]) {
-                        profile_img = $SCRIPT_ROOT + "/image/" + data.users[user][4];
-                    }
-                    $("#users").append("<div class='search_result'><i class='material-icons-outlined'>person</i><div class='search_result_top'><img src='" + profile_img + "'><div class='user_search_info'><h3>" + data.users[user][2] + " " + data.users[user][3] + "</h3></div></div><a href='/profile/" + data.users[user][1] + "' class='username'><span>" + data.users[user][1] + "</span></a></div>")
+        }
+        if (data.users) {
+            for (user in data.users) {
+                console.log(data.users[user][4]);
+                var profile_img = $SCRIPT_ROOT + "/static/img/avatar.png";
+                if (data.users[user][4]) {
+                    profile_img = $SCRIPT_ROOT + "/image/" + data.users[user][4];
                 }
-            };
-            $("nav .loader_container").removeClass("is_visible");
-        })
+                $("#users").append("<div class='search_result'><i class='material-icons-outlined'>person</i><div class='search_result_top'><img src='" + profile_img + "'><div class='user_search_info'><h3>" + data.users[user][2] + " " + data.users[user][3] + "</h3></div></div><a href='/profile/" + data.users[user][1] + "' class='username'><span>" + data.users[user][1] + "</span></a></div>")
+            }
+        };
+        $("nav .loader_container").removeClass("is_visible");
+    })
 };
 
 $("#search_container>a").click(function () {
--- static/js/profile.js
+++ static/js/profile.js
@@ -1,11 +1,13 @@
 window.onload = function () {
-
+    //Visar användare som personen följer
     $("#following_button").click(function () {
         $("#following").addClass("is_visible");
     });
+    //Visar användarens följare
     $("#followers_button").click(function () {
         $("#followers").addClass("is_visible");
     });
+    //Döljer followers/followings när användaren trycker kryss
     $("#following>i, #followers>i").click(function () {
         $("#following, #followers").removeClass("is_visible");
     });
@@ -50,8 +52,10 @@ window.onload = function () {
     $("#input_profile_img").change(function () {
         var file = this.files[0];
         var data = new FormData();
+        //Lägger till bild i FormData
         data.append("file", file);
 
+        //Skickar request till servern
         $.ajax({
             type: 'POST',
             url: $SCRIPT_ROOT + "/upload_profile_img",
@@ -60,16 +64,20 @@ window.onload = function () {
             contentType: false,
             processData: false,
             beforeSend: function () {
+                //Visar loading-snurra
                 $('.loader_container').addClass('is_visible');
             },
             success: function (data) {
+                //Ersätter profilbild till den nya och visar meddelande att det lyckades
                 add_flash_message("Profile image changed", "success");
                 $("#profile_img > img").attr("src", $SCRIPT_ROOT + "/image/" + data);
             },
             error: function (data) {
+                //Visar felmeddelande
                 add_flash_message("Could not change profile image...", "error");
             },
             complete: function () {
+                //Döljer loading-snurra
                 $('.loader_container').removeClass('is_visible');
             }
         });
@@ -83,8 +91,10 @@ window.onload = function () {
     $("#input_background_img").change(function () {
         var file = this.files[0];
         var data = new FormData();
+        //Lägger till bild i FormData
         data.append("file", file);
 
+        //Skickar request till servern
         $.ajax({
             type: 'POST',
             url: $SCRIPT_ROOT + "/upload_background_img",
@@ -93,16 +103,20 @@ window.onload = function () {
             contentType: false,
             processData: false,
             beforeSend: function () {
+                //Visar loading-snurra
                 $('.loader_container').addClass('is_visible');
             },
             success: function (data) {
+                //Ersätter bakgrundsbild till den nya och visar meddelande att det lyckades
                 add_flash_message("Background image changed", "success");
                 $("#profile_header").css("background-image", "url(" + $SCRIPT_ROOT + "/image/" + data + ")");
             },
             error: function (data) {
+                //Visar felmeddelande
                 add_flash_message("Could not change background image...", "error");
             },
             complete: function () {
+                //Döljer loading-snurra
                 $('.loader_container').removeClass('is_visible');
             }
         });
