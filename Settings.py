b2aca0e932274ef24b25d16df98114d17a635714
albinmedoc <stamline203@gmail.com>
2019-05-30 11:33:49 +0200

    Satte debug-mode till False

--- Settings.py
+++ Settings.py
@@ -5,7 +5,7 @@ SERVER_IP = "192.168.1.18"
 SERVER_PORT = 8080
 
 # Run server in debug-mode (True/False)
-DEBUG_MODE = True
+DEBUG_MODE = False
 
 # Path to folder where uploaded images will be stored
 UPLOAD_FOLDER = os.path.join(os.path.abspath(os.curdir) + "/images")
