a0b64d41bcf045b260fb9de8738080dd76e286d3
albinmedoc <stamline203@gmail.com>
2019-05-30 15:04:41 +0200

    Lade till kommentarer

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,6 +1,6 @@
-//Kontrollerar om username redan finns
 var timeout;
 $('#search').keyup(function () {
+    //Gör en sökning en halv sekund efter användaren har skrivit (detta eftersom vi vill undvika för många requests)
     if (timeout) {
         clearTimeout(timeout);
         timeout = null;
@@ -21,12 +21,13 @@ var do_search = function () {
     })
     .done(function (data) {
         $(".search_category").html('');
-        //Visar sökresultat
+        //Visar sökresultat för destinationer
         if (data.destinations) {
             for (destination in data.destinations) {
                 $("#destinations").append("<div class='search_result'><i class='material-icons-outlined'>place</i><a href='/album/" + data.destinations[destination][0] + "'><img src='/image/" + data.destinations[destination][5] + "'></a><div class='destinations_search_info'><a href='/album/" + data.destinations[destination][0] + "' class='city'><h3>" + data.destinations[destination][3] + "</h3></a><p>" + data.destinations[destination][2] + "</p><a class='album_owner' href='#'><i class='material-icons-outlined'>person</i><span>" + data.destinations[destination][4] + "</span></a></div></div>");
             }
         }
+        //Visar sökresultat för användare
         if (data.users) {
             for (user in data.users) {
                 console.log(data.users[user][4]);
@@ -40,15 +41,17 @@ var do_search = function () {
         $("nav .loader_container").removeClass("is_visible");
     })
 };
-
+//Döljer och visar sökresultat och sökruta
 $("#search_container>a").click(function () {
     if (!$(this).hasClass('is_visible')) {
         $("#search_results, #search, #search_container, #search_container>a").addClass("is_visible");
         $("#search").focus();
+        //Ändrar ikon till kryss och text till "Cancel"
         $("#search_container>a>i").html("close");
         $("#search_container>a>span").html("Cancel");
     } else if ($(this).hasClass('is_visible')) {
         $("#search_results, #search, #search_container, #search_container>a").removeClass("is_visible");
+        //Ändrar ikon till sök-ikon och text till "Search"
         $("#search_container>a>i").html("search");
         $("#search_container>a>span").html("Search");
     }
